a6c3a760019108939cd30d7527c101478c0f26f9
Valerio Pipolo <valep27@gmail.com>
2017-03-27 18:15:57 +0200

    Add input mapping

--- src/main.rs
+++ src/main.rs
@@ -48,31 +48,68 @@ fn run_emulator() -> Result<(), Box<std::error::Error>> {
     let mut emulator = Chip8::new();
 
     'running: loop {
-        // TODO: Run the chip8 here!
+        // Execute
         emulator.step();
 
-        // Drawing
+        // Draw
         renderer.clear();
         renderer.copy(&texture, None, None)?;
         renderer.present();
 
-        // Input handling
+        // Handle inputs
         for event in event_pump.poll_iter() {
-            match event {
+            let key_event : Option<chip8::core::KeyEvent> = match event {
                 Event::Quit {..} 
                 | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
                 Event::KeyDown { keycode: Some(key), .. } => {
                     println!("Key {} down", key);
+                    handle_input(true, key)
                 }
                 Event::KeyUp { keycode: Some(key), .. } => {
                     println!("Key {} up", key);
+                    handle_input(false, key)
                 }
-                _ => {}
+                _ => {None}
+            };
+
+            if let Some(event) = key_event {
+                emulator.handle_input(event);
             }
         }
     }
 
     Ok(())
+}
+
+fn handle_input(down: bool, key: sdl2::keyboard::Keycode) -> Option<chip8::core::KeyEvent> {
+    use sdl2::keyboard::Keycode;
+    use chip8::core::KeyEvent;
+
+    let keypad_num : usize = match key {
+        Keycode::Num1 => 1,
+        Keycode::Num2 => 2,
+        Keycode::Num3 => 3,
+        Keycode::Num4 => 3,
+        Keycode::Q => 5,
+        Keycode::W => 6,
+        Keycode::E => 7,
+        Keycode::R => 8,
+        Keycode::A => 9,
+        Keycode::S => 0xA,
+        Keycode::D => 0xB,
+        Keycode::F => 0xC,
+        Keycode::Z => 0xD,
+        Keycode::X => 0xE,
+        Keycode::C => 0,
+        Keycode::V => 0xF,
+        _ => return None,
+    };
+
+    if down {
+        Some(KeyEvent::Down(keypad_num))
+    } else {
+        Some(KeyEvent::Up(keypad_num))
+    }
 }
