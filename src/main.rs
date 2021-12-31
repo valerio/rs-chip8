23fb9c6c2ef2c23a11734f8d95968334231d6d9a
Valerio Pipolo <github@valerio.ws>
2021-12-31 16:20:24 +0100

    Switch from minifb to ggez 0.7

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,116 @@ mod chip8;
 
 use chip8::Chip8;
 use clap::{App, Arg};
-use minifb::{Key, Window, WindowOptions};
+use ggez::{
+    conf::WindowSetup,
+    event::{self, EventHandler, KeyCode},
+    graphics::{self, Color, DrawParam, Image},
+    input, timer, Context, ContextBuilder, GameResult,
+};
 
 use crate::chip8::KeyEvent;
 
 const WIDTH: usize = 64;
 const HEIGHT: usize = 32;
 
-fn keys_to_event_vec(keys: Option<Vec<Key>>) -> Vec<usize> {
-    keys.map_or(vec![], |keys| {
-        keys.iter()
-            .filter_map(|key| match key {
-                Key::Key1 => Some(0),
-                Key::Key2 => Some(1),
-                Key::Key3 => Some(2),
-                Key::Key4 => Some(3),
-                Key::Q => Some(4),
-                Key::W => Some(5),
-                Key::E => Some(6),
-                Key::R => Some(7),
-                Key::A => Some(8),
-                Key::S => Some(9),
-                Key::D => Some(10),
-                Key::F => Some(11),
-                Key::Z => Some(12),
-                Key::X => Some(13),
-                Key::C => Some(14),
-                Key::V => Some(15),
-                _ => None,
-            })
-            .collect()
-    })
+const KEYS: [KeyCode; 16] = [
+    KeyCode::Key1,
+    KeyCode::Key2,
+    KeyCode::Key3,
+    KeyCode::Key4,
+    KeyCode::Q,
+    KeyCode::W,
+    KeyCode::E,
+    KeyCode::R,
+    KeyCode::A,
+    KeyCode::S,
+    KeyCode::D,
+    KeyCode::F,
+    KeyCode::Z,
+    KeyCode::X,
+    KeyCode::C,
+    KeyCode::V,
+];
+
+fn keycode_to_event(key: KeyCode) -> usize {
+    match key {
+        KeyCode::Key1 => 0,
+        KeyCode::Key2 => 1,
+        KeyCode::Key3 => 2,
+        KeyCode::Key4 => 3,
+        KeyCode::Q => 4,
+        KeyCode::W => 5,
+        KeyCode::E => 6,
+        KeyCode::R => 7,
+        KeyCode::A => 8,
+        KeyCode::S => 9,
+        KeyCode::D => 10,
+        KeyCode::F => 11,
+        KeyCode::Z => 12,
+        KeyCode::X => 13,
+        KeyCode::C => 14,
+        KeyCode::V => 15,
+        _ => 0,
+    }
 }
 
-fn main() {
+struct EmulatorState {
+    emulator: Chip8,
+    fb: Vec<u8>,
+}
+
+impl EmulatorState {
+    pub fn new(emulator: Chip8) -> Self {
+        let fb = vec![0; WIDTH * HEIGHT * 4];
+        Self { emulator, fb }
+    }
+}
+
+impl EventHandler for EmulatorState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        for key in KEYS {
+            let input = if input::keyboard::is_key_pressed(ctx, key) {
+                KeyEvent::Up(keycode_to_event(key))
+            } else {
+                KeyEvent::Down(keycode_to_event(key))
+            };
+
+            self.emulator.handle_input(input);
+        }
+
+        while timer::check_update_time(ctx, 120) {
+            self.emulator.step();
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        if !self.emulator.should_draw() {
+            return Ok(());
+        }
+
+        graphics::clear(ctx, Color::WHITE);
+
+        let bw_framebuffer = self.emulator.get_framebuffer();
+        for i in 0..bw_framebuffer.len() {
+            let color = if bw_framebuffer[i] == 0 { 0 } else { 255 };
+            self.fb[(i * 4)] = color;
+            self.fb[(i * 4) + 1] = color;
+            self.fb[(i * 4) + 2] = color;
+            self.fb[(i * 4) + 3] = 255; // alpha
+        }
+
+        debug_assert_eq!(self.fb.len(), WIDTH * HEIGHT * 4);
+
+        let img = Image::from_rgba8(ctx, WIDTH as u16, HEIGHT as u16, &self.fb)?;
+        graphics::draw(ctx, &img, DrawParam::default())?;
+
+        graphics::present(ctx)
+    }
+}
+
+fn main() -> GameResult {
     let matches = App::new("rs-chip8")
         .about("Chip8 Emulator")
         .arg(
@@ -56,56 +132,29 @@ fn main() {
         .load_rom_file(file_name)
         .expect("Could not load file");
 
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    // Make a Context.
+    let (mut ctx, event_loop) = ContextBuilder::new("rs-chip8", "Valerio")
+        .window_setup(WindowSetup {
+            title: "Chip8".to_owned(),
+            ..WindowSetup::default()
+        })
+        .build()?;
 
-    let mut window = Window::new(
-        "Chip8",
-        WIDTH,
-        HEIGHT,
-        WindowOptions {
-            resize: true,
-            scale: minifb::Scale::X8,
-            scale_mode: minifb::ScaleMode::AspectRatioStretch,
-            ..Default::default()
-        },
-    )
-    .expect("Could not initialize window");
+    let state = EmulatorState::new(emulator);
 
-    // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    graphics::set_resizable(&mut ctx, true)?;
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        // handle keys pressed/released
-        for key in keys_to_event_vec(window.get_keys_pressed(minifb::KeyRepeat::No)) {
-            emulator.handle_input(KeyEvent::Down(key));
-        }
-        for key in keys_to_event_vec(window.get_keys_released()) {
-            emulator.handle_input(KeyEvent::Up(key));
-        }
+    // Set screen coordinates to match the size of a Chip8 framebuffer.
+    // This ensures that the framebuffer is stretched across the entire window.
+    graphics::set_screen_coordinates(
+        &mut ctx,
+        graphics::Rect::new(0.0, 0.0, WIDTH as f32, HEIGHT as f32),
+    )?;
 
-        // execute until the next frame should be drawn
-        while !emulator.should_draw() && !emulator.stopped {
-            emulator.step();
-        }
-        emulator.draw_flag = false;
+    // Set default filter mode to nearest-neighbor.
+    // This makes low resolution images look crisp as they are upscaled, if left to default
+    // Chip8 graphics would be smoothened out and look "blurry".
+    graphics::set_default_filter(&mut ctx, graphics::FilterMode::Nearest);
 
-        // prepare the framebuffer
-        let raw_fb = emulator.get_framebuffer();
-        for (i, pixel) in buffer.iter_mut().enumerate() {
-            if i == raw_fb.len() {
-                break;
-            }
-
-            *pixel = if raw_fb[i] == 0 {
-                0xFF000000
-            } else {
-                0xFFFFFFFF
-            };
-        }
-
-        // draw
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
-
-        // TODO: sound
-    }
+    event::run(ctx, event_loop, state);
 }
